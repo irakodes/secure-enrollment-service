c5bc46a9d12928df482160b3d07f5e6cea098126
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2025-12-19 19:02:55 +0200

    Inception: Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Secure Enrollment Service
---
+++ src/main/java/online/eracodes/secureenrollmentservice/SecureEnrollmentServiceApplication.java
@@ -0,0 +1,13 @@
+package online.eracodes.secureenrollmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureEnrollmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureEnrollmentServiceApplication.class, args);
+    }
+
+}
