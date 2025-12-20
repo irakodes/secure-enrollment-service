688ca7150858539318a4c3daa9a8486498d23d4b
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2025-12-20 19:34:57 +0200

    migrate: switch build system from Gradle to Maven IRA-113: Protobuf configurations

---
+++ src/main/java/online/eracodes/secureenrollmentservice/controller/UserController.java
@@ -0,0 +1,20 @@
+package online.eracodes.secureenrollmentservice.controller;
+
+import online.eracodes.protobuf.user.UserProto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @GetMapping(produces = "application/x-protobuf")
+    public UserProto.User getUser() {
+        return UserProto.User.newBuilder()
+                .setId(1)
+                .setName("John Doe")
+                .setEmail("john.doe@example.com")
+                .build();
+    }
+}
