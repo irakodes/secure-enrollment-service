8167cee0cd958930ce636d42bfe5e4e1e673e88b
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2025-12-21 00:45:59 +0200

    feature: Add commons-codec dependency and enhance MD5 hashing utilities in StringsUtil

--- src/main/java/online/eracodes/secureenrollmentservice/util/StringsUtil.java
+++ src/main/java/online/eracodes/secureenrollmentservice/util/StringsUtil.java
@@ -1,29 +1,43 @@
 package online.eracodes.secureenrollmentservice.util;
 
+import org.springframework.util.DigestUtils;
+
+import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 
 public class StringsUtil {
 
     /**
      * Hashes the registration code generated with MD5 Hash
+     *
      * @param registrationCode The generated code
      * @return An MD5 hash of the registration code
      */
-    private static String hashRegistrationCode(String registrationCode) {
-        throw new UnsupportedOperationException();
+    private static byte[] hashRegistrationCode(String registrationCode) {
+        return DigestUtils.md5Digest(registrationCode.getBytes(StandardCharsets.UTF_8));
     }
 
     /**
      * Handles MD5 Hash and returns the last two bytes of the hash
+     *
      * @param hashValue The MD5 Hash Value
      * @return The last two bytes of the MD5 Hash
      */
-    private static String md5HashHandler(String hashValue) {
-        throw new UnsupportedOperationException();
+    private static String md5HashHandler(byte[] hashValue) {
+        // Get the last two bytes of the MD5 Hash value
+        var checksum = String.format("%02x%02x", hashValue[hashValue.length - 2],
+                hashValue[hashValue.length - 1]);
+
+        if (checksum.isEmpty()) throw new IllegalArgumentException("Invalid registration code");
+        if (checksum.length() > 4) throw new IllegalArgumentException("Checksum length cannot exceed 4 characters");
+
+        // At this point I am sure the length is valid
+        return checksum;
     }
 
     /**
      * Generates a random hexadecimal code of the specified length.
+     *
      * @param length The length of the hexadecimal code to generate
      *               <b>Note:</b> The application currently expects 20 characters
      * @return A random hexadecimal code
