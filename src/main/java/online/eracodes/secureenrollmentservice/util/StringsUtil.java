62ac51396797fab6563e59c85bcceade0e2bebc3
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2025-12-21 00:33:59 +0200

    feature: Add StringsUtil class for generating registration codes and handling MD5-related operations

---
+++ src/main/java/online/eracodes/secureenrollmentservice/util/StringsUtil.java
@@ -0,0 +1,52 @@
+package online.eracodes.secureenrollmentservice.util;
+
+import java.security.SecureRandom;
+
+public class StringsUtil {
+
+    /**
+     * Hashes the registration code generated with MD5 Hash
+     * @param registrationCode The generated code
+     * @return An MD5 hash of the registration code
+     */
+    private static String hashRegistrationCode(String registrationCode) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Handles MD5 Hash and returns the last two bytes of the hash
+     * @param hashValue The MD5 Hash Value
+     * @return The last two bytes of the MD5 Hash
+     */
+    private static String md5HashHandler(String hashValue) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Generates a random hexadecimal code of the specified length.
+     * @param length The length of the hexadecimal code to generate
+     *               <b>Note:</b> The application currently expects 20 characters
+     * @return A random hexadecimal code
+     */
+    private static String generateHexadecimalCode(int length) {
+        if (length <= 0) length = 8;
+        var secureRandom = new SecureRandom();
+        var byteCount = (length + 1) / 2;
+        var randomBytes = new byte[byteCount];
+        secureRandom.nextBytes(randomBytes);
+
+        var hex = new StringBuilder(byteCount * 2);
+        for (var b : randomBytes) {
+            hex.append(String.format("%02x", b));
+        }
+
+        return hex.substring(0, length);
+    }
+
+    public static String generateRegistrationCode(String username) {
+        var usernameInHex = String.format("%08x", username.hashCode());
+        var code = generateHexadecimalCode(8);
+
+        return usernameInHex + code;
+    }
+}
