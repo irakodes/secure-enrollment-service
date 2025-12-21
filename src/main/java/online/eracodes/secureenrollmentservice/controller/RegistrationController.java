b61a5908086fcf4e94640299bb6208cd7dbdacc6
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2025-12-21 03:10:06 +0200

    feature: Add Login protobuf definition and create RegistrationController for handling registration requests

---
+++ src/main/java/online/eracodes/secureenrollmentservice/controller/RegistrationController.java
@@ -0,0 +1,10 @@
+package online.eracodes.secureenrollmentservice.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+    private final static Logger LOG = LoggerFactory.getLogger(RegistrationController.class);
+}
