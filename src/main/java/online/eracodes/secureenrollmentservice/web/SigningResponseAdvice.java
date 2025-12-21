c94ea73d8d233bbd46aac2007e5ed847e9120104
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2025-12-21 14:43:00 +0200

    feature: Implement SigningResponseAdvice for protobuf message handling; add response body advice methods for enhanced response processing

--- src/main/java/online/eracodes/secureenrollmentservice/web/SigningResponseAdvice.java
+++ src/main/java/online/eracodes/secureenrollmentservice/web/SigningResponseAdvice.java
@@ -1,4 +1,31 @@
 package online.eracodes.secureenrollmentservice.web;
 
-public class SigningResponseAdvice {
+import com.google.protobuf.Message;
+import org.jspecify.annotations.Nullable;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class SigningResponseAdvice implements ResponseBodyAdvice<Message> {
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return false;
+    }
+
+    @Override
+    public @Nullable Message beforeBodyWrite(@Nullable Message body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        return null;
+    }
+
+    @Override
+    public @Nullable Map<String, Object> determineWriteHints(@Nullable Message body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType) {
+        return ResponseBodyAdvice.super.determineWriteHints(body, returnType, selectedContentType, selectedConverterType);
+    }
 }
