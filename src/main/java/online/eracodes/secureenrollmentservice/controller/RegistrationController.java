86c17b9c0345e2cf7624146c9b111ffa3453c2c1
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2025-12-21 03:14:12 +0200

    feature: Implement initial registration endpoint in RegistrationController with placeholder logic

--- src/main/java/online/eracodes/secureenrollmentservice/controller/RegistrationController.java
+++ src/main/java/online/eracodes/secureenrollmentservice/controller/RegistrationController.java
@@ -1,10 +1,23 @@
 package online.eracodes.secureenrollmentservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import online.eracodes.protobuf.enrollment.EnrollmentProto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/register")
 public class RegistrationController {
     private final static Logger LOG = LoggerFactory.getLogger(RegistrationController.class);
+
+    @PostMapping("/")
+    public ResponseEntity<?> registerUser(@RequestBody EnrollmentProto.CreateUserRequest request) {
+        throw new UnsupportedOperationException("Registration endpoint not yet implemented.");
+    }
 }
